91f4a890079699f77efafd4a9d9f9eed5daa6a2d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-19 18:30:47 +0530

    Add files via upload

---
+++ src/main/java/com/iiht/evaluation/automation/Activities.java
@@ -0,0 +1,32 @@
+package com.iiht.evaluation.automation;
+
+import org.openqa.selenium.WebDriver;
+
+public class Activities {
+    
+    public static boolean click_product_product_image_in_arrival(WebDriver driver, String product_name) {
+        return false;
+    }
+    public static int get_number_of_item_in_cart(WebDriver driver){
+        return -1;
+    }
+    public static boolean click_add_to_basket_button_product_detail_page(WebDriver driver) {
+        return false;
+    }
+    public static boolean check_product_added_message_product_detail_page(WebDriver driver, String product_name) {
+        return false;
+    }
+
+    public static boolean click_item_detail_menu(WebDriver driver){
+        return false;
+    }
+
+   
+
+    public static boolean check_product_total_checkout_page(WebDriver driver, String product_name){
+        return false;
+    }
+
+}
+
+
